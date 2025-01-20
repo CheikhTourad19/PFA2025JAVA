849a94ffc7c1f5931b164b7de07ef95006257886
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-01-20 17:33:38 +0100

    first try

--- src/main/java/pfa/java/pfa2025java/HelloController.java
+++ src/main/java/pfa/java/pfa2025java/HelloController.java
@@ -2,13 +2,21 @@ package pfa.java.pfa2025java;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    private int num=0;
+    public Image image;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        num++;
+        welcomeText.setText("Welcome to JavaFX Application!"+num);
+    }
+    @FXML
+    protected void onclikini(){
+        welcomeText.setText("Baraaaaa neyek bataaaaaaard");
+
     }
 }
