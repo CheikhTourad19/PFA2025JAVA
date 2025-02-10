38630f820047e1c53a191275510148291ccd68fe
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-02-10 19:16:45 +0100

    barcha

--- src/main/java/pfa/java/pfa2025java/controllers/pharmacie/OrdonnanceController.java
+++ src/main/java/pfa/java/pfa2025java/controllers/pharmacie/OrdonnanceController.java
@@ -1,4 +1,20 @@
 package pfa.java.pfa2025java.controllers.pharmacie;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import pfa.java.pfa2025java.SwtichScene;
+
 public class OrdonnanceController {
+
+    public Button stockButton;
+    public Button accueilButton;
+
+
+    public void gotostock(ActionEvent actionEvent) {
+    }
+
+    public void gotoaccueil(ActionEvent actionEvent) {
+        SwtichScene swtichScene = new SwtichScene();
+        swtichScene.loadScene(actionEvent,"views/pharmacie/accueil-view.fxml","Accueil",false);
+    }
 }
