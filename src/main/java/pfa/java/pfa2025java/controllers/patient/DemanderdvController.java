ff2cbb784100cdb6881b923f8714935b5d28d1bc
ALI0benjannet <alialilou54098546@gmail.com>
2025-02-19 14:46:52 +0100

    ali-page mes rdv encore

--- src/main/java/pfa/java/pfa2025java/controllers/patient/DemanderdvController.java
+++ src/main/java/pfa/java/pfa2025java/controllers/patient/DemanderdvController.java
@@ -1,57 +1,53 @@
 package pfa.java.pfa2025java.controllers.patient;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import pfa.java.pfa2025java.SwtichScene;
 import pfa.java.pfa2025java.UserSession;
+import pfa.java.pfa2025java.model.RendezVous;
+import java.util.Date;
 
-public class AccueilController {
-
+public class DemanderdvController {
     @FXML
     private Text Bienvenu;
 
     @FXML
-    private Button accueilButton;
+    private TableView<RendezVous> medicamentTable;
 
     @FXML
-    private ComboBox<?> cityComboBox;
+    private TableColumn<RendezVous, String> mednameCol;
 
     @FXML
-    private ComboBox<?> countryComboBox;
+    private TableColumn<RendezVous, String> quantiteCol;
 
-    @FXML
-    private ComboBox<?> genderComboBox;
+    private ObservableList<RendezVous> rendezVousList = FXCollections.observableArrayList();
 
-    @FXML
-    private ProgressIndicator loading;
+    public void initialize() {
+        Bienvenu.setText(UserSession.getPrenom() + " " + UserSession.getNom());
 
-    @FXML
-    private TableView<?> medicamentTable;
+        // Initialisation des colonnes de la table
+        mednameCol.setCellValueFactory(new PropertyValueFactory<>("medecinNom"));
+        quantiteCol.setCellValueFactory(new PropertyValueFactory<>("etat"));
 
-    @FXML
-    private TableColumn<?, ?> mednameCol;
+        // Chargement des rendez-vous
+        chargerRendezVous();
+    }
 
-    @FXML
-    private TableColumn<?, ?> quantiteCol;
+    private void chargerRendezVous() {
+        // Simulation des rendez-vous (remplace avec une récupération depuis la base de données)
+        rendezVousList.add(new RendezVous(1, 101, "Dr. Mohamed", 202, "Ali Ben Jannet", new Date(), "Confirmé"));
+        rendezVousList.add(new RendezVous(2, 102, "Dr. Yassine", 202, "Ali Ben Jannet", new Date(), "En attente"));
 
-    @FXML
-    private Button searchButton;
+        // Mise à jour de la table
+        medicamentTable.setItems(rendezVousList);
+    }
 
-    @FXML
-    private Button searchDoctorsButton;
-
-    @FXML
-    private TextField searchField;
-
-    @FXML
-    private ComboBox<?> specialtyComboBox;
 
     @FXML
 
@@ -74,7 +70,6 @@ public class AccueilController {
         SwtichScene swtichScene = new SwtichScene();
         swtichScene.loadScene(actionEvent, "views/patient/pharmacie-view.fxml", "Pharmacies", false);
     }
-
     public void logout(ActionEvent actionEvent) {
         SwtichScene swtichScene = new SwtichScene();
         swtichScene.loadScene(actionEvent, "views/hello-view.fxml", "Login", false);
