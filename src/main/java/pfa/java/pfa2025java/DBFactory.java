de4611e09a7646fc31d8fb80b66e528e2aae8019
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-02-22 13:55:03 +0100

    Changed the database credentiels

--- src/main/java/pfa/java/pfa2025java/DBFactory.java
+++ src/main/java/pfa/java/pfa2025java/DBFactory.java
@@ -4,13 +4,14 @@ import com.github.javafaker.Faker;
 import pfa.java.pfa2025java.model.MedicamentDAO;
 
 public class DBFactory {
-    private static final Faker faker = new Faker();
+    private static  Faker faker = new Faker();
 
     public static void main(String[] args) {
 //        for (int i = 0; i < 100; i++) {
 //            MedicamentDAO.addMedicamentWithStock(faker.name().firstName(),faker.name().firstName(),"100",2);
+//            System.out.println("crree");
 //        }
-//        for (int i = 1; i <109 ; i++) {
+//        for (int i = 1319; i < 1414; i++) {
 //            MedicamentDAO.ajouterStock(i,faker.number().numberBetween(10,100),2);
 //        }
     }
