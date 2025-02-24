49be7128c83bb23d5597e4674d3fbd6ddd52953c
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-02-24 15:16:38 +0100

    fema chkoun bedel esm dossier fxml Medecin

--- src/main/java/pfa/java/pfa2025java/controllers/medecin/util/NavigationUtil.java
+++ src/main/java/pfa/java/pfa2025java/controllers/medecin/util/NavigationUtil.java
@@ -6,7 +6,7 @@ import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
 
 public class NavigationUtil {
-    private static final String VIEWS__PATH = "/pfa/java/pfa2025java/views/medecin/";
+    private static final String VIEWS__PATH = "/pfa/java/pfa2025java/views/Medecin/";
 
     public static void navigateTo(AnchorPane container, String viewName) {
         try {
