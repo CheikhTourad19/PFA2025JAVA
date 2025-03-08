cccac1bf7c385cb5f7ac4f0ff1152b418d494250
cheikh tourad <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-03-08 23:20:50 +0100

    Tout le projet est maintenant Single Page Application

--- src/main/java/pfa/java/pfa2025java/controllers/patient/SidebarController.java
+++ src/main/java/pfa/java/pfa2025java/controllers/patient/SidebarController.java
@@ -2,6 +2,7 @@ package pfa.java.pfa2025java.controllers.patient;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -12,11 +13,34 @@ public class SidebarController {
 
     public Text nomUtil;
     public VBox mainContent;
+    public Button accueilButton;
+    public Button ordonnanceButton;
+    public Button rdvButton;
+    public Button pharmaButton;
+    public Button demandeButton;
+    public Button chatButton;
+    private Button activeButton;
+
 
     public void initialize() {
         nomUtil.setText(UserSession.getNom() + " " + UserSession.getPrenom());
         NavigationUtil.navigateTo(mainContent, "profile", this);
+        setActiveButton(accueilButton);
 
+    }
+
+    private void setActiveButton(Button button) {
+        // Réinitialiser les styles des boutons
+        ordonnanceButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        chatButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        accueilButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        rdvButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        demandeButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        pharmaButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+
+        // Appliquer un style au bouton actif
+        button.setStyle(" -fx-cursor: hand; -fx-background-color: #3498db; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;"); // Bleu clair
+        activeButton = button;
     }
 
     public void logout(ActionEvent actionEvent) {
@@ -35,25 +59,32 @@ public class SidebarController {
 
     public void demandeRendezVous(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "demanderdv", this);
+        setActiveButton(demandeButton);
     }
 
     public void chat(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "chatbot", this);
+        setActiveButton(chatButton);
     }
 
     public void consulterPharmacies(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "pharmacie", this);
+        setActiveButton(pharmaButton);
     }
 
     public void consulterOrdonnances(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "ordonnance", this);
+        setActiveButton(ordonnanceButton);
     }
 
     public void consulterProfil(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "profile", this);
+        setActiveButton(accueilButton);
+
     }
 
     public void consulterRDV(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "accueil", this);
+        setActiveButton(rdvButton);
     }
 }
--- src/main/java/pfa/java/pfa2025java/controllers/pharmacie/SidebarController.java
+++ src/main/java/pfa/java/pfa2025java/controllers/pharmacie/SidebarController.java
@@ -2,6 +2,7 @@ package pfa.java.pfa2025java.controllers.pharmacie;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -12,19 +13,37 @@ import pfa.java.pfa2025java.UserSession;
 public class SidebarController {
     public VBox mainContent;
     public Text name;
+    public Button ordonnanceButton;
+    public Button stockButton;
+    public Button accueilButton;
+    private Button activeButton;
 
     public void initialize() {
         NavigationUtil.navigateTo(mainContent, "accueil", this);
         name.setText(UserSession.getNom() + " " + UserSession.getPrenom());
+        setActiveButton(accueilButton);
 
+    }
+
+    private void setActiveButton(Button button) {
+        // Réinitialiser les styles des boutons
+        ordonnanceButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        stockButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+        accueilButton.setStyle(" -fx-cursor: hand; -fx-background-color: #34495E; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;");
+
+        // Appliquer un style au bouton actif
+        button.setStyle(" -fx-cursor: hand; -fx-background-color: #3498db; -fx-text-fill: white; -fx-pref-width: 180; -fx-font-size: 14px; -fx-font-weight: bold;"); // Bleu clair
+        activeButton = button;
     }
 
     public void gotoordonnance(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "ordonnance", this);
+        setActiveButton(ordonnanceButton);
     }
 
     public void gotostock(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "stock", this);
+        setActiveButton(stockButton);
     }
 
     public void logout(ActionEvent actionEvent) {
@@ -43,5 +62,6 @@ public class SidebarController {
 
     public void gotoaccueil(ActionEvent actionEvent) {
         NavigationUtil.navigateTo(mainContent, "accueil", this);
+        setActiveButton(accueilButton);
     }
 }
