983ed9b7b789687be7e5bb83494a218ec3237b1d
Ihab <160348546+ChouaibiIheb@users.noreply.github.com>
2025-03-20 06:46:04 +0100

    creation des task need infirmiere interface

--- src/main/java/pfa/java/pfa2025java/constant/MessageRequet.java
+++ src/main/java/pfa/java/pfa2025java/constant/MessageRequet.java
@@ -5,24 +5,27 @@ public  class MessageRequet {
     public static  String  getMsgBetweenUsers="SELECT *, (vu IS FALSE AND sender_id != ?) AS is_unread FROM messages " +
             "WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) " +
             "ORDER BY sent_at ASC";
-    public static final String getUsersWithLastMsg = "SELECT u.id , u.prenom, m.content, m.sent_at, m.vu, m.sender_id " +
-            "FROM user u " +
-            "JOIN messages m ON (u.id = m.sender_id OR u.id = m.receiver_id) " +
-            "WHERE (m.sender_id = ? OR m.receiver_id = ?) AND u.id != ? " +
-            "AND m.id = ( " +
-            "    SELECT MAX(id) " +
-            "    FROM messages " +
-            "    WHERE (sender_id = u.id AND receiver_id = ?) OR (sender_id = ? AND receiver_id = u.id) " +
-            ") " +
-            "ORDER BY m.sent_at DESC";
-    public static  String  search_Users=    "SELECT u.id, u.prenom, " +
-            "SUBSTRING_INDEX(GROUP_CONCAT(m.content ORDER BY m.sent_at DESC), ',', 1) AS content, " +
-            "MAX(m.sent_at) AS sent_at, MAX(m.vu) AS vu, MAX(m.sender_id) AS sender_id " +
-            "FROM user u " +
-            "LEFT JOIN messages m ON (u.id = m.sender_id OR u.id = m.receiver_id) " +
-            "WHERE u.prenom LIKE ? AND u.id != ? " +
-            "GROUP BY u.id, u.prenom " +
-            "ORDER BY sent_at DESC";
+    public static final String getUsersWithLastMsg =
+            "SELECT u.id, CONCAT(u.nom, ' ', u.prenom) AS fullname, m.content, m.sent_at, m.vu, m.sender_id " +
+                    "FROM user u " +
+                    "JOIN messages m ON (u.id = m.sender_id OR u.id = m.receiver_id) " +
+                    "WHERE (m.sender_id = ? OR m.receiver_id = ?) AND u.id != ? " +
+                    "AND m.id = ( " +
+                    "    SELECT MAX(id) " +
+                    "    FROM messages " +
+                    "    WHERE (sender_id = u.id AND receiver_id = ?) OR (sender_id = ? AND receiver_id = u.id) " +
+                    ") " +
+                    "ORDER BY m.sent_at DESC";
+
+    public static  String  search_Users=
+            "SELECT u.id, CONCAT(u.nom, ' ', u.prenom) AS fullname, " +
+                    "SUBSTRING_INDEX(GROUP_CONCAT(m.content ORDER BY m.sent_at DESC), ',', 1) AS content, " +
+                    "MAX(m.sent_at) AS sent_at, MAX(m.vu) AS vu, MAX(m.sender_id) AS sender_id " +
+                    "FROM user u " +
+                    "LEFT JOIN messages m ON (u.id = m.sender_id OR u.id = m.receiver_id) " +
+                    "WHERE (u.prenom LIKE ? OR u.nom LIKE ?) AND u.id != ? AND u.role = 'medecin' " + // Ajout du filtre
+                    "GROUP BY u.id, u.prenom, u.nom " +
+                    "ORDER BY sent_at DESC";
     public static  String  getNewMsg= "SELECT * FROM messages " +
             "WHERE ((sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)) " +
             "AND sent_at > ?";
---
+++ src/main/java/pfa/java/pfa2025java/controllers/medecin/TasksMenu.java
@@ -0,0 +1,53 @@
+package pfa.java.pfa2025java.controllers.medecin;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class TasksMenu {
+    @FXML private BorderPane mainLayout;
+    @FXML private AnchorPane contentPane;
+
+    public void initialize() {
+        loadDefaultView();
+    }
+
+    private void loadDefaultView() {
+        loadView("/pfa/java/pfa2025java/views/medecin/task/createtask-view.fxml");
+    }
+
+    private void loadView(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            AnchorPane view = loader.load();
+
+            contentPane.getChildren().setAll(view);
+            AnchorPane.setTopAnchor(view, 0.0);
+            AnchorPane.setBottomAnchor(view, 0.0);
+            AnchorPane.setLeftAnchor(view, 0.0);
+            AnchorPane.setRightAnchor(view, 0.0);
+
+        } catch (IOException e) {
+            System.err.println("Erreur de chargement: " + fxmlPath);
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void loadCreateTask() {
+        loadView("/pfa/java/pfa2025java/views/medecin/task/createtask-view.fxml");
+    }
+
+    @FXML
+    public void loadMyTasks() {
+        loadView("/pfa/java/pfa2025java/views/medecin/task/mytask-view.fxml");
+    }
+
+    @FXML
+    public void loadFollowedTasks() {
+        loadView("/pfa/java/pfa2025java/views/medecin/task/followed-tasks-view.fxml");
+    }
+}
